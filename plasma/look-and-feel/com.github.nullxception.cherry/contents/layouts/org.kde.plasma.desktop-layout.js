dc7b7032fd3507547f644846ecf8da707f9626e2
Nauval Rizky <enuma.alrizky@gmail.com>
2020-11-25 00:11:52 +0700

    look-and-feel : update sensible default layout
    apply several widgets dynamically and also create a "dock" based on
    plasma panel if latte-dock is not yet installed
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- plasma/look-and-feel/com.github.nullxception.cherry/contents/layouts/org.kde.plasma.desktop-layout.js
+++ plasma/look-and-feel/com.github.nullxception.cherry/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,152 +1,63 @@
-var plasma = getApiVersion(1);
+var panel = new Panel()
+// Panel setup
+panel.height = Math.ceil(gridUnit * 2.5 / 2) * 1.4
+panel.location = "top"
 
-var layout = {
-    "desktops": [
-        {
-            "applets": [
-            ],
-            "config": {
-                "/": {
-                    "ItemGeometriesHorizontal": "",
-                    "formfactor": "0",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "540",
-                    "DialogWidth": "720"
-                },
-                "/Configuration": {
-                    "PreloadWeight": "42"
-                },
-                "/General": {
-                    "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "462",
-                    "ToolBoxButtonY": "32"
-                },
-                "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///usr/share/wallpapers/Elarun/contents/images/2560x1600.png"
-                }
-            },
-            "wallpaperPlugin": "org.kde.image"
-        }
-    ],
-    "panels": [
-        {
-            "alignment": "left",
-            "applets": [
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "100"
-                        },
-                        "/Configuration/General": {
-                            "favoritesPortedToKAstats": "true",
-                            "systemApplications": "systemsettings.desktop,org.kde.kinfocenter.desktop"
-                        },
-                        "/Configuration/Shortcuts": {
-                            "global": "Alt+F1"
-                        },
-                        "/Shortcuts": {
-                            "global": "Alt+F1"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.kickoff"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "42"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.appmenu"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "42"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.panelspacer"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "52"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.systemtray"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "72"
-                        },
-                        "/Configuration/Appearance": {
-                            "displayTimezoneAsCode": "false",
-                            "spinboxHorizontalPercentage": "60",
-                            "use24hFormat": "2"
-                        },
-                        "/Configuration/ConfigDialog": {
-                            "DialogHeight": "540",
-                            "DialogWidth": "720"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.splitdigitalclock"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "52"
-                        }
-                    },
-                    "plugin": "org.kde.milou"
-                }
-            ],
-            "config": {
-                "/": {
-                    "formfactor": "2",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "74",
-                    "DialogWidth": "1366"
-                },
-                "/Configuration": {
-                    "PreloadWeight": "42"
-                }
-            },
-            "height": 1.7777777777777777,
-            "hiding": "normal",
-            "location": "top",
-            "maximumLength": 75.88888888888889,
-            "minimumLength": 75.88888888888889,
-            "offset": 0
-        }
-    ],
-    "serializationFormatVersion": "1"
+// App Launcher
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+
+// App Title
+if (knownWidgetTypes.indexOf('org.kde.windowtitle') > -1) {
+    var wTitle = panel.addWidget("org.kde.windowtitle")
+    wTitle.writeConfig("boldFont", true)
+    wTitle.writeConfig("filterByScreen", true)
+    wTitle.writeConfig("placeHolder", "Cherry Desktop")
+    wTitle.writeConfig("showIcon", false)
 }
-;
 
-plasma.loadSerializedLayout(layout);
+// App Menu
+if (knownWidgetTypes.indexOf('org.kde.windowappmenu') > -1) {
+    panel.addWidget("org.kde.windowappmenu")
+} else {
+    panel.addWidget("org.kde.plasma.appmenu")
+}
+
+// Spacer to push tray and the rest to the right side
+panel.addWidget("org.kde.plasma.panelspacer")
+
+// Tray
+panel.addWidget("org.kde.plasma.systemtray")
+
+// Clock
+if (knownWidgetTypes.indexOf('org.kde.plasma.betterinlineclock') > -1) {
+    var clock = panel.addWidget("org.kde.plasma.betterinlineclock")
+    clock.writeConfig("fixedFont", true)
+    clock.writeConfig("fontSize", Math.round(panel.height * 0.45))
+    clock.writeConfig("showDate", false)
+    clock.writeConfig("showSeparator", false)
+} else {
+    var clock = panel.addWidget("org.kde.plasma.digitalclock")
+    clock.writeConfig("showDate", false)
+}
+
+// User Switch
+var uswitch = panel.addWidget("org.kde.plasma.userswitcher")
+uswitch.writeConfig("showFace", true)
+uswitch.writeConfig("showFullName", false)
+uswitch.writeConfig("showName", false)
+uswitch.writeConfig("showTechnicalInfo", true)
+
+// Create sensible "dock" using plasma panel if latte-dock is not exists
+if (!applicationExists("latte-dock")) {
+    var dock = new Panel()
+    dock.alignment = "center"
+    dock.height = 50
+    dock.hiding = "windowscover"
+    dock.location = "bottom"
+    dock.minimumLength = gridUnit
+
+    var tasks = dock.addWidget("org.kde.plasma.icontasks")
+    tasks.writeConfig("maxStripes", 1)
+}
