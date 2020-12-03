e95f1012c5f3cc53e2286b5587b5695fd3c24d65
Nauval Rizky <enuma.alrizky@gmail.com>
2020-12-03 14:29:10 +0700

    pling-pkg: version the package using git tag
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- scripts/pling-pkgs.sh
+++ scripts/pling-pkgs.sh
@@ -3,6 +3,7 @@ name=cherry
 src=$(realpath "$(dirname "$0")/..")
 dist=$src/dist
 tmp=$(mktemp -d /tmp/nullxception.kde-$name-XXX)
+tag=$(git tag)
 
 rm -rf $dist; mkdir $dist
 chmod +x $src/install.sh
@@ -12,19 +13,19 @@ PREFIX=$tmp $src/install.sh >/dev/null
 
 # Packaging all the components
 cd $tmp/share/aurorae/themes
-tar -czvf $dist/$name-aurorae.tar.gz . >/dev/null
+tar -czvf $dist/$name-aurorae-$tag.tar.gz . >/dev/null
 cd $tmp/share/konsole
-tar -czvf $dist/$name-konsole.tar.gz . >/dev/null
+tar -czvf $dist/$name-konsole-$tag.tar.gz . >/dev/null
 cd $tmp/share/color-schemes
-tar -czvf $dist/$name-color-schemes.tar.gz . >/dev/null
+tar -czvf $dist/$name-color-schemes-$tag.tar.gz . >/dev/null
 for v in cherry{,-solid}; do
   cd $tmp/share/plasma/desktoptheme
-  tar -czvf $dist/$v-plasma.tar.gz $v >/dev/null
+  tar -czvf $dist/$v-plasma-$tag.tar.gz $v >/dev/null
 done
 cd $tmp/share/plasma/look-and-feel
-tar -czvf $dist/$name-look-and-feel.tar.gz . >/dev/null
+tar -czvf $dist/$name-look-and-feel-$tag.tar.gz . >/dev/null
 cd $tmp/share/wallpapers
-tar -czvf $dist/$name-wallpaper.tar.gz . >/dev/null
+tar -czvf $dist/$name-wallpaper-$tag.tar.gz . >/dev/null
 
 # cleaning up
 rm -rf $tmp
