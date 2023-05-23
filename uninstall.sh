9cfcd90df738ddfc6b260129b63d6e721d542b99
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-23 15:44:04 +0700

    update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-THEME_NAME=cherry
-color_schemes=(Cherry)
-
-if [[ $EUID -ne 0 ]]; then
-  PREFIX="$HOME/.local"
-elif [[ -z "$PREFIX" ]]; then
-  PREFIX=/usr
-fi
-
-# Destination directory
-AURORAE="$PREFIX/share/aurorae/themes"
-KONSOLE="$PREFIX/share/konsole"
-KVANTUM="$PREFIX/share/Kvantum"
-LOOKFEEL="$PREFIX/share/plasma/look-and-feel"
-PLASMA="$PREFIX/share/plasma/desktoptheme"
-SCHEMES="$PREFIX/share/color-schemes"
-WALLPAPER="$PREFIX/share/wallpapers"
-# Special Kvantum dest for user-specific install
-[[ $EUID -ne 0 ]] && KVANTUM="$HOME/.config/Kvantum"
-
 del() {
   if [[ -e "$1" ]]; then
     echo "path found, removing $1"
@@ -29,14 +9,42 @@ del() {
   fi
 }
 
-del ${AURORAE}/${THEME_NAME}
-del ${AURORAE}/${THEME_NAME}-solid
-del ${AURORAE}/${THEME_NAME}-square
-del ${AURORAE}/${THEME_NAME}-square-solid
-del ${KVANTUM}/${THEME_NAME}
-del ${KVANTUM}/${THEME_NAME}-solid
-del ${LOOKFEEL}/com.github.nullxception.${THEME_NAME}
-del ${PLASMA}/${THEME_NAME}
-del ${PLASMA}/${THEME_NAME}-solid
-del ${SCHEMES}/${color_schemes[0]}.colors
-del ${WALLPAPER}/${THEME_NAME}
+main() {
+  for variant in cherry cherry-midnight cherry-midnight-solid cherry-midnight-square cherry-midnight-square-solid cherry-solid cherry-square cherry-square-solid; do
+    del "$PREFIX/share/aurorae/themes/$variant"
+  done
+
+  local KVANTUM="$PREFIX/share/Kvantum"
+  # Special Kvantum dest for user-specific install
+  [[ $EUID -ne 0 ]] && KVANTUM="$HOME/.config/Kvantum"
+
+  for variant in cherry cherry-midnight cherry-midnight-solid cherry-solid; do
+    del "$KVANTUM/$variant"
+  done
+
+  for variant in cherry cherrymidnight; do
+    del "$PREFIX/share/plasma/look-and-feel/com.github.nullxception.${variant}"
+  done
+
+  for variant in cherry cherry-midnight cherry-midnight-solid cherry-solid; do
+    del "$PREFIX/share/plasma/desktoptheme/$variant"
+  done
+
+  for variant in cherry cherry-midnight; do
+    del "$PREFIX/share/konsole/${variant}.colorscheme"
+  done
+
+  for variant in Cherry CherryMidnight; do
+    del "$PREFIX/share/color-schemes/${variant}.colors"
+  done
+
+  del "$PREFIX/share/wallpapers/cherry"
+}
+
+if [[ $EUID -ne 0 ]]; then
+  PREFIX="$HOME/.local"
+elif [[ -z "$PREFIX" ]]; then
+  PREFIX=/usr
+fi
+
+main "$@"
